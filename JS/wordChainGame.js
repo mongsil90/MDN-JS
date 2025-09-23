ad38856df9e2c151e00392cd567d4589a855dffc
saja <saja90@naver.com>
2025-09-23 16:16:13 +0900

    wordChainGame Done.

--- JS/wordChainGame.js
+++ JS/wordChainGame.js
@@ -1,13 +1,103 @@
-// 요소 가져오기
-const suggestedWord = document.querySelector(".suggestedWord");
+// 인원 수 요소 가져오기
+const startForm = document.querySelector(".startForm");
+const headCount = document.querySelector(".headCount");
 const startButton = document.querySelector(".startButton");
+const userNumber = document.querySelector(".userNumber");
+
+// 단어 입력 요소 가져오기
+const wordGameForm = document.querySelector(".wordGameForm");
+const wordInput = document.querySelector(".wordInput");
+const wordSubmit = document.querySelector(".wordSubmit");
+const lastWords = document.querySelector(".lastWords");
+
+const gameOver = document.querySelector(".gameOver");
+
+//
+let startCount = 1;
+let newWord;
+let pastWord;
+let resetButton;
+
+headCount.focus();
 
 
-function startGame(event) {
-    event.preventDefault();
-    const startWord = suggestedWord.value;
-    console.log(startWord);
+// 리셋 게임
+function resetGame() {
+    wordInput.disabled = false;
+    wordSubmit.disabled = false;
+
+    userNumber.textContent = "";
+    lastWords.textContent = "";
+    gameOver.textContent = "";
+
+    resetButton.remove();
+}
+
+// 리셋 버튼 생성
+function resetBtn() {
+    resetButton = document.createElement("button");
+    resetButton.textContent = "Reset Game 🚀";
+    document.body.append(resetButton);
+    resetButton.addEventListener("click", resetGame);
 }
 
 
+// testWord 함수
+function testWord() {
+
+    if (pastWord.at(-1) !== newWord.at(0)) {
+        wordInput.disabled = true;
+        wordSubmit.disabled = true;
+        gameOver.textContent = "Game Over..!!";
+        resetBtn();
+    }
+}
+
+
+// 단어 게임 시작
+function checkWord(e) {
+    e.preventDefault();
+
+    // 만약 전 단어가 비었으면 채우기
+    if (!newWord) {
+        newWord = wordInput.value;
+    } else {
+        pastWord = newWord;
+        newWord = wordInput.value;
+        testWord();
+    } 
+
+    lastWords.textContent += wordInput.value + " > ";
+    wordInput.value = "";
+    setUsercount();
+    
+}
+
+// 단어 입력 시 몇 번째 차례인지 알려주는 함수
+function setUsercount() {
+    
+    userNumber.textContent = `[ ${startCount} ] 번 참가자의 차례입니다!`;
+    startCount++
+
+    const userCount = Number(headCount.value);
+    if (startCount > userCount) {
+        startCount = 1;
+    }
+}
+
+// 콜백: 게임 시작 함수
+function startGame(e) {
+    e.preventDefault();
+
+    startForm.style.display = "none";
+    wordGameForm.classList.toggle("show");
+    setUsercount();
+    wordSubmit.addEventListener("click", checkWord);
+    wordInput.focus();
+}
+
+
+
+
+// 이벤트 리스너: 게임 시작
 startButton.addEventListener("click", startGame);
