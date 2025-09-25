1107d7b94421fdc72f6de51f6425308bf645dca9
saja <saja90@naver.com>
2025-09-25 17:00:27 +0900

    9월 25일 쿵쿵따

--- JS/wordChainGame.js
+++ JS/wordChainGame.js
@@ -3,13 +3,14 @@ const startForm = document.querySelector(".startForm");
 const headCount = document.querySelector(".headCount");
 const startButton = document.querySelector(".startButton");
 const userNumber = document.querySelector(".userNumber");
+const users = document.querySelector(".users");
 
 // 단어 입력 요소 가져오기
 const wordGameForm = document.querySelector(".wordGameForm");
 const wordInput = document.querySelector(".wordInput");
 const wordSubmit = document.querySelector(".wordSubmit");
 const lastWords = document.querySelector(".lastWords");
-const gameOver = document.querySelector(".gameOver");
+//const gameOver = document.querySelector(".gameOver");
 
 //
 let startCount = 1;
@@ -21,8 +22,12 @@ let words = [];
 headCount.focus();
 
 
+
 // 리셋 버튼 생성 함수
 function resetBtn() {
+    wordInput.disabled = true;
+    wordSubmit.disabled = true;
+
     resetButton = document.createElement("button");
     resetButton.textContent = "Reset Game 🚀";
     document.body.append(resetButton);
@@ -31,80 +36,75 @@ function resetBtn() {
     });
 }
 
-// 중복 단어 제한 함수
-function checkRepWord() {
-    const checkRep = words.find((word) => word === newWord);
-    if (checkRep) {
-        wordInput.disabled = true;
-        wordSubmit.disabled = true;
-        gameOver.textContent = "중복된 단어입니다!";
-        resetBtn();
-    }
-}
-
-
-// 앞 단어의 끝 글자로 시작하는 단어인가?
-function testWord() {
-    if (pastWord.at(-1) !== newWord.at(0)) {
-        wordInput.disabled = true;
-        wordSubmit.disabled = true;
-        gameOver.textContent = "잘못된 단어입니다!";
-        resetBtn();
-    }
-
-}
-
-// 3글자 제한 함수
-function wordLengthLimit() {
-    if (newWord.length !== 3) {
-        wordInput.disabled = true;
-        wordSubmit.disabled = true;
-        gameOver.textContent = "3글자 단어가 아닙니다!";
-        resetBtn();
-    }
-}
 
 // 단어 저장 함수
-function savedWord() {
-    
+function checkWord() {
+    startCount++
     // 만약 전 단어가 비었으면 채우기
     if (!newWord) {
         newWord = wordInput.value;
         words.push(newWord);
+
+        if (newWord.length !== 3) {
+            userNumber.textContent = `3글자 단어가 아닙니다! [ ${startCount} ]번 참가자, 탈락!`;
+            resetBtn();
+        }
+        
     } else {
         pastWord = newWord;
         newWord = wordInput.value;
-        testWord();
-        checkRepWord();
+        
+        const checkRep = words.find((word) => word === newWord);
+    
+        if (checkRep) {
+            userNumber.textContent = `중복된 단어입니다! [ ${startCount} ]번 참가자, 탈락!`;
+            resetBtn();
+        } else if (pastWord.at(-1) !== newWord.at(0)) {
+            userNumber.textContent = `잘못된 단어입니다! [ ${startCount} ]번 참가자, 탈락!`;
+            resetBtn();
+        } else if (newWord.length !== 3) {
+            userNumber.textContent = `3글자 단어가 아닙니다! [ ${startCount} ]번 참가자, 탈락!`;
+            resetBtn();
+        }
     } 
-    wordLengthLimit();
+    
     
     lastWords.textContent += wordInput.value + " > ";
     wordInput.value = "";
-    setUsercount();
 }
+
 
 // 단어 입력 시 몇 번째 차례인지 알려주는 함수
 function setUsercount() {
-
+    console.log(startCount);
     userNumber.textContent = `[ ${startCount} ] 번 참가자의 차례입니다!`;
-    startCount++
-
+    checkWord();
+    
+    
     const userCount = Number(headCount.value);
     if (startCount > userCount) {
         startCount = 1;
     }
 }
 
+
 // 콜백: 게임 시작 함수
 function startGame(e) {
     e.preventDefault();
 
+    if (Number(headCount.value) === 0) {
+        alert("1명 이상의 인원 수를 숫자로 입력하세요!");
+        location.reload();
+    }
+    
     startForm.style.display = "none";
     wordGameForm.classList.toggle("show");
-    setUsercount();
-    wordSubmit.addEventListener("click", savedWord);
+    users.textContent = `참가자: ${headCount.value}명`;
+    userNumber.textContent = `[ ${startCount} ] 번 참가자의 차례입니다!`;
+    startCount++
+    wordSubmit.addEventListener("click", setUsercount);
     wordInput.focus();
+    
 }
 
 
